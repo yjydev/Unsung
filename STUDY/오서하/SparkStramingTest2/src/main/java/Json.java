5dcbdda5ff8aeebdbf3d4e848934d25d2d15bd1e
오서하 <seoha8952@gmail.com>
2022-03-29 21:44:13 +0900

    feat: 🎸 newratio 계산

---
+++ STUDY/오서하/SparkStramingTest2/src/main/java/Json.java
@@ -0,0 +1,13 @@
+public class Json {
+    private String label;
+
+    private String[] keywordList;
+
+    public Json(){
+    }
+
+    public Json(String label, String[] keywordList) {
+        this.label = label;
+        this.keywordList = keywordList;
+    }
+}
